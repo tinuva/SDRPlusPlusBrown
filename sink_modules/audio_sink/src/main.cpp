af254a6a3acf9829c5170befb529193afde34619
Sanny Sanoff <san@sysdate.com>
2024-02-05 11:31:43 +0100

    audio merge fix

--- sink_modules/audio_sink/src/main.cpp
+++ sink_modules/audio_sink/src/main.cpp
@@ -52,7 +52,7 @@ public:
         stereoPacker.init(_stream->sinkOut, 512);
 
 #if RTAUDIO_VERSION_MAJOR >= 6
-        audio.setErrorCallback(&reportErrorsAsException);
+        audio.setErrorCallback(&errorCallback);
 #endif
 
         bool created = false;
